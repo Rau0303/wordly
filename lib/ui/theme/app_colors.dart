d430291f6e3b20a8d3c4c4850fdffdffcde463a8
Рауан Айтанатов <rauan@MacBook-Air-Rauan.local>
2025-08-18 18:05:32 +0500

    app colors

---
+++ lib/ui/theme/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Основной цвет приложения
+  static const Color primary = Color(0xFFFD232D);
+
+  // Элементы интерфейса
+  static const Color graphite = Color(0xFF302F2D);
+  static const Color darkGray = Color(0xFF202020);
+  static const Color lightGray = Color(0xFFF5F5F5);
+
+  // Фон
+  static const Color blackBackground = Color(0xFF000000);
+  static const Color whiteBackground = Color(0xFFFFFFFF);
+}
