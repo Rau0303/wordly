15bc56533282dc8ee46953124af043d28a7ff824
Рауан Айтанатов <rauan@MacBook-Air-Rauan.local>
2025-08-18 17:54:04 +0500

    initial commit [dependencies]

---
+++ android/app/src/main/kotlin/com/rauan/wordly/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rauan.wordly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app/app.dart
@@ -0,0 +1,4 @@
+export 'app_config.dart';
+export 'app_initializer.dart';
+export 'repository_container.dart';
+export 'wordly_app.dart';
